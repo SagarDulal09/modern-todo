dbc5f40a7e55c3672166125eb94ba9287cfbbe64
Sagar Dulal <133942125+SagarDulal09@users.noreply.github.com>
2026-04-17 16:11:29 +0545

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 // js/api.js
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_URL_HERE"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbxBnJA-bySZumgVenlMExqcVZUsUSdo3Efxywx_lHL6pFeNXQ6Uxg4bUz2fwKYw9ohJaw/exec"; 
 
 async function apiRequest(data) {
     const response = await fetch(API_URL, {
